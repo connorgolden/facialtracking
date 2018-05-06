81763a0c8743b65d2fa741b5e3c56b36cfa896a9
connorgolden <connor@connorgolden.me>
2018-05-06 16:55:27 -0700

    Picture Taking

--- app/src/main/java/me/connorgolden/facialtracking/MainActivity.java
+++ app/src/main/java/me/connorgolden/facialtracking/MainActivity.java
@@ -1,18 +1,30 @@
 package me.connorgolden.facialtracking;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -23,6 +35,9 @@ import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import butterknife.BindView;
@@ -33,6 +48,7 @@ import me.connorgolden.facialtracking.ui.camera.GraphicOverlay;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private static final int PERMISSION_REQUEST_CODE = 1;
     @BindView(R.id.camera)
     CameraSourcePreview cameraView;
 
@@ -41,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
 
     private CameraSource cameraSource = null;
     private boolean isFrontFacing = false;
+    private boolean showEmoji = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +67,13 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        if (checkStoragePermission() == false){
+            requestStoragePermission();}
+
         createCameraSource();
         startCameraSource();
+
+
 
         ImageButton switchCamButton = findViewById(R.id.switchCameraButton);
         switchCamButton.setOnClickListener(new View.OnClickListener() {
@@ -79,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         camButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Call Take Picture
+                takePicture();
             }
         });
 
@@ -187,8 +210,188 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void takePicture(){
+        try{
+            cameraSource.takePicture(null, new CameraSource.PictureCallback() {
+                private File imageFile;
+                @Override
+                public void onPictureTaken(byte[] bytes) {
+                    Log.d("TakePicture", "Picture Taken");
+                    Bitmap loadedImage = null;
+                    Bitmap rotatedBitmap = null;
+                    loadedImage = BitmapFactory.decodeByteArray(bytes, 0,
+                            bytes.length);
+
+                    // rotate Image
+                    Log.v("TakePicture", String.valueOf(getWindowManager().getDefaultDisplay().getRotation()));
+
+                    //int degrees = getWindowManager().getDefaultDisplay().getRotation() + 90;
+
+                    int rotation = getWindowManager().getDefaultDisplay().getRotation();
+                    int degree = 0;
+
+                    Log.v("rotation", String.valueOf(camRotation()));
+
+                    Matrix rotateMatrix = new Matrix();
+                    rotateMatrix.postRotate(camRotation());
+                    rotatedBitmap = Bitmap.createBitmap(loadedImage, 0, 0,
+                            loadedImage.getWidth(), loadedImage.getHeight(),
+                            rotateMatrix, false);
+
+
+
+                    String state = Environment.getExternalStorageState();
+                    File folder = null;
+                    if (state.contains(Environment.MEDIA_MOUNTED)) {
+                        folder = new File(Environment
+                                .getExternalStorageDirectory() + "/DCIM/Camera/");
+                    } else {
+                        folder = new File(Environment
+                                .getExternalStorageDirectory() + "/DCIM/Camera/");
+                    }
+
+                    Log.v("Path", folder.toString());
+
+                    boolean success = true;
+                    if (!folder.exists()) {
+                        success = folder.mkdirs();
+                    }
+                    if (success) {
+                        java.util.Date date = new java.util.Date();
+                        imageFile = new File(folder.getAbsolutePath()
+                                + File.separator
+                                //+ new Timestamp(date.getTime()).toString()
+                                + "Image.jpg");
+                        try {
+                            imageFile.createNewFile();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Toast.makeText(getBaseContext(), "Image Not saved",
+                                Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+
+                    // save image into gallery
+                    rotatedBitmap = resize(rotatedBitmap, 800, 600);
+                    rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+
+                    try {
+                        FileOutputStream fout = new FileOutputStream(imageFile);
+                        fout.write(ostream.toByteArray());
+                        fout.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    ContentValues values = new ContentValues();
+
+                    values.put(MediaStore.Images.Media.DATE_TAKEN,
+                            System.currentTimeMillis());
+                    values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                    values.put(MediaStore.MediaColumns.DATA,
+                            imageFile.getAbsolutePath());
+
+                    MainActivity.this.getContentResolver().insert(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+                    setResult(Activity.RESULT_OK); //add this
+
+                }
+            });
+        }catch (Exception e){
+            Log.e("TakePicture", e.toString());
+        }
+    }
+
+    private Bitmap resize(Bitmap image, int maxWidth, int maxHeight) {
+        if (maxHeight > 0 && maxWidth > 0) {
+            int width = image.getWidth();
+            int height = image.getHeight();
+            float ratioBitmap = (float) width / (float) height;
+            float ratioMax = (float) maxWidth / (float) maxHeight;
+
+            int finalWidth = maxWidth;
+            int finalHeight = maxHeight;
+            if (ratioMax > 1) {
+                finalWidth = (int) ((float) maxHeight * ratioBitmap);
+            } else {
+                finalHeight = (int) ((float) maxWidth / ratioBitmap);
+            }
+            image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
+            return image;
+        } else {
+            return image;
+        }
+    }
+
+    private boolean checkStoragePermission() {
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(MainActivity.this, "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("value", "Permission Granted, Now you can use local drive .");
+                } else {
+                    Log.e("value", "Permission Denied, You cannot use local drive .");
+                }
+                break;
+        }
+    }
+
     protected void launchSettings(){
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+
+    private int camRotation() {
+
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        int degree = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degree = 90;
+                break;
+            case Surface.ROTATION_90:
+                degree = -180;
+                break;
+            case Surface.ROTATION_180:
+                degree = 270;
+                break;
+            case Surface.ROTATION_270:
+                degree = 0;
+                break;
+
+            default:
+                break;
+        }
+
+       /* if (CameraSource.f == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            // frontFacing
+            rotation = (info.orientation + degree) % 330;
+            rotation = (360 - rotation) % 360;
+        } else {
+            // Back-facing
+            rotation = (rotation - degree + 360) % 360;
+        }
+        */
+
+        return degree;
+    }
+
 }
