53e5fd73cd118f6c969b4f2b1774621eaf63ec3d
connorgolden <connor@connorgolden.me>
2018-05-06 00:59:52 -0700

    Devtest

--- app/src/main/java/me/connorgolden/facialtracking/MainActivity.java
+++ app/src/main/java/me/connorgolden/facialtracking/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //DevTest
         ButterKnife.bind(this);
 
         cameraView.setMethod(cameraMethod);
