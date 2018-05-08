2d78ff9e29123e92b3e7e8a5d4d1645d098b7938
byJD <jochem.dierx@gmail.com>
2018-05-08 00:23:01 -0700

    FaceData clean up

--- app/src/main/java/me/connorgolden/facialtracking/FaceData.java
+++ app/src/main/java/me/connorgolden/facialtracking/FaceData.java
@@ -23,22 +23,6 @@ public class FaceData {
   private boolean mRightEyeOpen;
   private boolean mSmiling;
 
-  // Facial landmarks
-  private PointF mLeftEyePosition;
-  private PointF mRightEyePosition;
-  private PointF mLeftCheekPosition;
-  private PointF mRightCheekPosition;
-  private PointF mNoseBasePosition;
-  private PointF mLeftEarPosition;
-  private PointF mLeftEarTipPosition;
-  private PointF mRightEarPosition;
-  private PointF mRightEarTipPosition;
-  private PointF mMouthLeftPosition;
-  private PointF mMouthBottomPosition;
-  private PointF mMouthRightPosition;
-
-
-
 
   public int getId() {
     return mId;
@@ -110,102 +94,6 @@ public class FaceData {
 
   public void setSmiling(boolean smiling) {
     this.mSmiling = smiling;
-  }
-
-  public PointF getLeftEyePosition() {
-    return mLeftEyePosition;
-  }
-
-  public void setLeftEyePosition(PointF leftEyePosition) {
-    this.mLeftEyePosition = leftEyePosition;
-  }
-
-  public PointF getRightEyePosition() {
-    return mRightEyePosition;
-  }
-
-  public void setRightEyePosition(PointF rightEyePosition) {
-    this.mRightEyePosition = rightEyePosition;
-  }
-
-  public PointF getLeftCheekPosition() {
-    return mLeftCheekPosition;
-  }
-
-  public void setLeftCheekPosition(PointF leftCheekPosition) {
-    mLeftCheekPosition = leftCheekPosition;
-  }
-
-  public PointF getRightCheekPosition() {
-    return mRightCheekPosition;
-  }
-
-  public void setRightCheekPosition(PointF rightCheekPosition) {
-    mRightCheekPosition = rightCheekPosition;
-  }
-
-  public PointF getNoseBasePosition() {
-    return mNoseBasePosition;
-  }
-
-  public void setNoseBasePosition(PointF noseBasePosition) {
-    this.mNoseBasePosition = noseBasePosition;
-  }
-
-  public PointF getLeftEarPosition() {
-    return mLeftEarPosition;
-  }
-
-  public void setLeftEarPosition(PointF leftEarPosition) {
-    mLeftEarPosition = leftEarPosition;
-  }
-
-  public PointF getLeftEarTipPosition() {
-    return mLeftEarTipPosition;
-  }
-
-  public void setLeftEarTipPosition(PointF leftEarTipPosition) {
-    mLeftEarTipPosition = leftEarTipPosition;
-  }
-
-  public PointF getRightEarPosition() {
-    return mRightEarPosition;
-  }
-
-  public void setRightEarPosition(PointF rightEarPosition) {
-    mRightEarPosition = rightEarPosition;
-  }
-
-  public PointF getRightEarTipPosition() {
-    return mRightEarTipPosition;
-  }
-
-  public void setRightEarTipPosition(PointF rightEarTipPosition) {
-    mRightEarTipPosition = rightEarTipPosition;
-  }
-
-  public PointF getMouthLeftPosition() {
-    return mMouthLeftPosition;
-  }
-
-  public void setMouthLeftPosition(PointF mouthLeftPosition) {
-    this.mMouthLeftPosition = mouthLeftPosition;
-  }
-
-  public PointF getMouthBottomPosition() {
-    return mMouthBottomPosition;
-  }
-
-  public void setMouthBottomPosition(PointF mouthBottomPosition) {
-    this.mMouthBottomPosition = mouthBottomPosition;
-  }
-
-  public PointF getMouthRightPosition() {
-    return mMouthRightPosition;
-  }
-
-  public void setMouthRightPosition(PointF mouthRightPosition) {
-    this.mMouthRightPosition = mouthRightPosition;
   }
 
 }
